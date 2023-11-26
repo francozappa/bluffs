2b8201cfee28a708f9cf32a46d2378b83f712430
francozappa <antonioli.daniele@gmail.com>
2023-11-26 18:14:37 +0100

    Add man_ac

---
+++ device/man_ac.s
@@ -0,0 +1,12 @@
+@LSC AC to zero
+and r0, r0, #0x0
+str.w r0, [r4, #0x78]
+str.w r0, [r4, #0x7C]
+str.w r0, [r4, #0x80]
+str.w r0, [r4, #0x84]
+
+@Execute missed instruction
+add.w r2, r4, #0x78
+
+@Jump to trigger_addr + 5
+b 0xAEB91
